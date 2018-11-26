b5815dfb56f9b11c24ab0f79506a18521d0d7d39
Artur112 <36160815+Artur112@users.noreply.github.com>
2018-11-26 02:06:16 +0000

    Update
    Added morphological closing with 1 size diamond shape as a step for imageJ to run before segmentation. Also created code that prepares samples for neural network by correcting pixel labels and removing edges and bodies of smaller regions to discard them.

--- ProjectMain.m
+++ ProjectMain.m
@@ -190,8 +190,97 @@ fclose(file);
 MIJ.run('MorphSegmentation');
 MIJ.run('Close All');
 
-
-
+for pic = 1:numpics
+        X = imread("C:\Users\artur\Desktop\PICTURES\picture" + num2str(pic) + ".tif");
+        
+        %Swap ones (background) and zeros(edges) in output from imagej to
+        %ones for edges and zeros for background
+        X2 = X;
+        for m = 1:size(X,1)
+            for n = 1:size(X,2)
+                if(X(m,n) == 0)
+                    X2(m,n) = 1;
+                elseif(X(m,n) == 1)
+                    X2(m,n) = 0;
+                end
+            end
+        end
+        
+        %Remove edges from image
+        X3 = X2;
+        for m = 1:size(X2,1)
+            for n = 1:size(X2,2)
+                if(X2(m,n) == 1)
+                    X3(m,n) = 0;
+                end
+            end
+        end
+        
+        %Find areas and positions of the cells / fragments
+        %with edges removed
+        ka = regionprops(X3);
+        if(pic == 1)
+            k2 = ka;
+            figure;
+            imagesc(X2);
+        end
+        Area = [ka.Area];
+        Centroids = {ka.Centroid};
+        BoundingBoxes = {ka.BoundingBox};
+        X4 = X2;
+        
+        %Loop through the areas found
+        for blob = 1:length(Area)
+            %If its area is smaller than 50, discard it, by looping through all the cells
+            %inside the bounding box and deleting those that are equal to
+            %the class label (value of cell at centroid). Looping through
+            %the bounding box instead of the whole image was done for
+            %speed.
+            if(Area(blob) < 50 && Area(blob) ~= 0)
+               labl = X2(floor(Centroids{blob}(2)), round(Centroids{blob}(1)));
+               for m = floor(Centroids{blob}(2)-BoundingBoxes{blob}(4)/2): round(Centroids{blob}(2) + BoundingBoxes{blob}(4)/2)
+                   for n = floor(Centroids{blob}(1)-BoundingBoxes{blob}(3)/2): round(Centroids{blob}(1) + BoundingBoxes{blob}(3)/2)
+                       %Dont check outside image
+                       if(m<301 && n<301)
+                           if(X2(m,n) == labl)
+                               X4(m,n) = 0;
+                           end
+                       end
+                   end
+               end
+            end
+        end
+        
+        %Deleting the remaining edges of the globs that were removed. Done
+        %by deleting every edges that isnt next to a class label.
+        X5 = X4;
+        for m = 1:size(X5,1)
+            for n = 1:size(X5,2)
+                if(X5(m,n) == 1)
+                    nums = 0;
+                    for a = -1:1
+                        for b = -1:1
+                            %dont check outside edges
+                            if(m+a < 301 && n+b < 301)
+                                if(X5(m+a,n+b) > 1)
+                                    nums = nums + 1;
+                                end
+                            end
+                        end
+                    end
+                    if(nums == 0)
+                        X5(m,n) = 0;
+                    end
+                end
+            end
+        end      
+        if(pic == 1)
+            figure;
+            imagesc(X5);
+        end
+        imwrite(X5,"C:\users\artur\Desktop\PICTURES\blobsremoved" + num2str(pic) + ".jpg");
+end
+            
 %% Section for performing analysis on all images on all images at once.
 %Reading in all the image files 
 for k = 1:157
